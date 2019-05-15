6148e19560bd1c113e40c78f02163dc477a9bf07
FreeCodersix <949578388@qq.com>
2019-05-15 17:50:41 +0800

    d

--- out/artifacts/2048_war_exploded/JS/index.js
+++ out/artifacts/2048_war_exploded/JS/index.js
@@ -1,4 +1,5 @@
-var board = new Array();
+var board = new Array();//存储随机生生成的数字
+var added = new Array();//记录当前位置是否完成过合并
 var score = 0;
 
 $(document).ready(function (e) {
@@ -22,9 +23,17 @@ function init() {
             board[i][j] = 0;
         }
     }
+
+    for (var i = 0; i < 4; i++) {
+        added[i] = new Array();
+        for (var j = 0; j < 4; j++) {
+            added[i][j] = 0;
+        }
+    }
+
     randomNum();
     randomNum();
-    updateThePage();//更新游戏界面。
+    updateThePage();
 }
 
 function getPosTop(i) {
@@ -35,7 +44,7 @@ function getPosLeft(j) {
     return 20 + j * 120;
 }
 
-function updateThePage() {//更新数组的前端样式
+function updateThePage() {//更新游戏界面。
     $(".number-grid").remove();
     for (var i = 0; i < 4; i++) {
         for (var j = 0; j < 4; j++) {
@@ -81,23 +90,17 @@ function randomNum() {//生成随机的格子
 $(document).keydown(function (event) {
     switch (event.keyCode) {
         case 37://left
-            //每次移动都需要生成一个数字
-
-            for (var i = 0; i < 4; i++) {
-                for (var j = 0; j < 4; j++) {
-                    if (board[i][j]!==0)
-                    moveAnimation(i, j);
-                }
+            if (moveLeft()) {
+                //每次移动都需要生成一个数字
+                randomNum();
             }
-            randomNum();
-
             break;
         case 38://up
             //每次移动都需要生成一个数字
             randomNum();
             for (var i = 0; i < 4; i++) {
                 for (var j = 0; j < 4; j++) {
-                    if (board[i][j]!==0)
+                    if (board[i][j] !== 0)
                         moveAnimation(i, j);
                 }
             }
@@ -107,7 +110,7 @@ $(document).keydown(function (event) {
             randomNum();
             for (var i = 0; i < 4; i++) {
                 for (var j = 0; j < 4; j++) {
-                    if (board[i][j]!==0)
+                    if (board[i][j] !== 0)
                         moveAnimation(i, j);
                 }
             }
@@ -117,7 +120,7 @@ $(document).keydown(function (event) {
             randomNum();
             for (var i = 0; i < 4; i++) {
                 for (var j = 0; j < 4; j++) {
-                    if (board[i][j]!==0)
+                    if (board[i][j] !== 0)
                         moveAnimation(i, j);
                 }
             }
@@ -125,11 +128,70 @@ $(document).keydown(function (event) {
     }
 });
 
-function moveAnimation(x,y) {//实现移动格子的样式变动
+function moveLeft() {
+    if (!canMoveLeft(board))
+        return false;
+
+    addedReset();
+    //函数核心
+    for (var i = 0; i < 4; i++)
+        for (var j = 1; j < 4; j++) {//第一列的数字不可能向左移动
+            if (board[i][j] != 0) {
+                //(i,j)左侧的元素
+                for (var k = 0; k < j; k++) {
+                    //落脚位置的是否为空 && 中间没有障碍物
+                    if (board[i][k] == 0 && noBlockHorizontal(i, j, k, board)) {
+                        //满足条件就移动
+                        moveAnimation(i, j, i, k);
+                        board[i][k] = board[i][j];
+                        board[i][j] = 0;
+                        continue;
+                    }
+                    //落脚位置的数字和本来的数字相等 && 中间没有障碍物
+                    else if (board[i][k] == board[i][j] && noBlockHorizontal(i, j, k, board)) {
+                        moveAnimation(i, j, i, k);
+                        //add
+                        board[i][k] += board[i][j];
+                        board[i][j] = 0;
+                        added[i][k] = 1;//此位置已经合并过，避免一次按键触发多次事件
+
+                        continue;
+                    }
+                }
+            }
+        }
+    setTimeout("updateThePage()", 200);
+    return true;
+}
+
+function addedReset() {
+    for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 4; j++) {
+            added[i][j] = 0;
+        }
+    }
+}
+
+function canMoveLeft() {
+    for (var i = 0; i < 4; i++)
+        for (var j = 1; j < 4; j++)
+            if (board[i][j] != 0 && (board[i][j - 1] == 0 || board[i][j - 1] == board[i][j]))
+                return true;
+    return false;
+}
+
+function moveAnimation(x, y) {//实现移动格子的样式变动
 
     var numberGrid = $('#number-grid-' + x + '-' + y);
     numberGrid.animate({
         top: getPosTop(x),
         left: 20
     }, 200);
+}
+
+function noBlockHorizontal(row, col1, col2, board){
+    for(var i = col2 + 1; i<col1; i++)
+        if(board[row][i]!=0)
+            return false;
+    return true;
 }
